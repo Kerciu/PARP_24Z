2bd81cf0d99dbeac6d879f1b9d8fcff026b6cdac
Kerciu <Kerciuuu@gmail.com>
2024-12-10 20:24:00 +0100

    filled game state data type with flags and inventory field which is a list of interactables

--- haskell/src/GameState.hs
+++ haskell/src/GameState.hs
@@ -1,7 +1,16 @@
 module GameState where
 import Locations
+import Interactable
 
 data GameState = GameState {
     currentLocation :: Location,
+    inventory :: [Interactable],
+    drunkardInteraction :: Bool,
+    firstHomelessInteraction :: Bool,
+    secondHomelessInteraction :: Bool,
+    escapeCityEnding :: Bool,
+    hillChurchEndingEscape :: Bool,
+    forestCaveEndingKilled :: Bool,
+    forestCaveEndingWeakened :: Bool,
     gameOver :: Bool
 }
