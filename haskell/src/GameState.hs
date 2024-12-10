05ee94fcab52adb2db4fb22782e5ffd0c9b086d0
Kerciu <Kerciuuu@gmail.com>
2024-12-10 21:08:57 +0100

    moved initial state to the game state module and printLines function to the utils module

--- haskell/src/GameState.hs
+++ haskell/src/GameState.hs
@@ -14,3 +14,17 @@ data GameState = GameState {
     forestCaveEndingWeakened :: Bool,
     gameOver :: Bool
 }
+
+initialState :: GameState
+initialState = GameState {
+    currentLocation = trainStation,
+    inventory = [],
+    drunkardInteraction = False,
+    firstHomelessInteraction = False,
+    secondHomelessInteraction = False,
+    escapeCityEnding = False,
+    hillChurchEndingEscape = False,
+    forestCaveEndingKilled = False,
+    forestCaveEndingWeakened = False,
+    gameOver = False
+}
