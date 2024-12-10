0e86cd699dd121f81508946e1c9afe2c573341bc
mandrysz <01187204@pw.edu.pl>
2024-12-10 21:55:28 +0000

    Added dropItem function

--- haskell/src/Inventory.hs
+++ haskell/src/Inventory.hs
@@ -2,6 +2,7 @@ module Inventory where
 
 import GameState
 import Interactable
+import CommandParser
 
 -- Handle taking an item
 takeItem :: String -> GameState -> IO GameState
@@ -24,4 +25,27 @@ takeItem itemName state =
     where
         noItemFound itemName = do
             putStrLn $ "There is no '" ++ itemName ++ "' here."
+            return state
+
+-- Handle dropping an item
+dropItem :: String -> GameState -> IO GameState
+dropItem itemName state =
+    case findItem itemName of
+        Nothing -> noItemFound itemName
+        Just item ->
+            if item `elem` inventory state
+            then do
+                putStrLn $ "You have dropped the " ++ name item ++ "."
+                let updatedLocation = (currentLocation state) {
+                        locationItems = item : locationItems (currentLocation state)
+                    }
+                let updatedState = state {
+                        currentLocation = updatedLocation,
+                        inventory = filter (/= item) (inventory state)
+                    }
+                return updatedState
+            else noItemFound itemName
+    where
+        noItemFound itemName = do
+            putStrLn $ "You don't have the '" ++ itemName ++ "' in your inventory."
             return state
