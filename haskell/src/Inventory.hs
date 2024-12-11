0fda9bec90088dac4747c8c8cca924f73f9d6fa3
mandrysz <01187204@pw.edu.pl>
2024-12-11 00:03:50 +0000

    Added displayItemDescription function

--- haskell/src/Inventory.hs
+++ haskell/src/Inventory.hs
@@ -3,6 +3,7 @@ module Inventory where
 import GameState
 import Interactable
 import CommandParser
+import Distribution.InstalledPackageInfo (InstalledPackageInfo(description))
 
 -- Handle taking an item
 takeItem :: String -> GameState -> IO GameState
@@ -12,7 +13,6 @@ takeItem itemName state =
         Just item ->
             if item `elem` locationItems (currentLocation state)
             then do
-                putStrLn $ "You have taken the " ++ name item ++ "."
                 let updatedLocation = (currentLocation state) {
                         locationItems = filter (/= item) (locationItems $ currentLocation state)
                     }
@@ -20,11 +20,13 @@ takeItem itemName state =
                         currentLocation = updatedLocation,
                         inventory = item : inventory state
                     }
+                putStrLn $ "You have taken the " ++ name item ++ "."
+                putStrLn & description item
                 return updatedState
             else noItemFound itemName
     where
         noItemFound itemName = do
-            putStrLn $ "There is no '" ++ itemName ++ "' here."
+            putStrLn $ "There is no " ++ itemName ++ " here."
             return state
 
 -- Handle dropping an item
@@ -47,7 +49,7 @@ dropItem itemName state =
             else noItemFound itemName
     where
         noItemFound itemName = do
-            putStrLn $ "You don't have the '" ++ itemName ++ "' in your inventory."
+            putStrLn $ "You don't have the " ++ itemName ++ " in your inventory."
             return state
 
 -- Handle checking inventory
@@ -58,3 +60,16 @@ showInventory state =
         items -> do
             putStrLn "You are carrying the following items:"
             putStrLn (unlines (map name items))
+
+-- Display description of an item if it is in inventory
+displayItemDescription :: String -> GameState -> IO ()
+displayItemDescription itemName state =
+    case findItem itemName of
+        Nothing -> noItemFound itemName
+        Just item ->
+            if item `elem` inventory state
+            then putStrLn $ description item
+            else noItemFound itemName
+    where
+        noItemFound itemName = 
+           putStrLn $ "You are not holding the " ++ itemName ++ "." 
