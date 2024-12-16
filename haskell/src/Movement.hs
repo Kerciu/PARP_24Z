30d8e5e63c839a2294e0a06f9b86797175db829a
mandrysz <01187204@pw.edu.pl>
2024-12-16 01:00:06 +0000

    Minor changes

--- haskell/src/Movement.hs
+++ haskell/src/Movement.hs
@@ -63,56 +63,6 @@ move strDirection currentLocationStr state =
         Nothing ->
           putStrLn "You can't go that way" >> return state
 
--- goNextLocation :: String -> GameState -> IO GameState
--- goNextLocation "hill_church" state = do
---   putStrLn "You stand before an old, abandoned church on the hill."
---   putStrLn "The dark interior and the smell of incense remind you of ancient rituals."
---   putStrLn "The priest, the last witness of the former life in the city, looks at you with an expression of concern."
---   putStrLn "You can interact with him by interact priest"
---   if hasItem "amulet" state
---     then do
---       putStrLn "The priest notices the amulet in your hand and warns you: That is the symbol of their cult; do not approach them with it."
---       putStrLn "He continues: I can''t tell you more, just be careful"
---       putStrLn "He points at the old staircase, at the west side of the church, that leads to second floor of the church"
---       putStrLn "The path to the forest is to the north."
---     else do
---       putStrLn "The path to the forest is to the north."
---   let updatedState = state {currentLocation = hillChurch}
---   return updatedState
--- goNextLocation "archive" state = do
---   putStrLn "You enter the archive room. It is small and cramped, filled with stacks of old papers and documents."
---   putStrLn "Most of the documents are unreadable due to age."
---   if isItemAt greenFuse "archive" state
---     then do
---       putStrLn "However you notice a bright green fuse lying on a nearby shelf."
---     else do
---       putStrLn ""
---   let updatedState = state {currentLocation = hillChurch}
---   return updatedState
--- goNextLocation "parking" state = do
---   putStrLn "You are in a deserted parking lot near the train station. The ground is littered"
---   putStrLn "with old tickets and rusted cans. One car looks abandoned, as if the driver left in a hurry."
---   putStrLn "To the north, you can see a homeless man sitting on a bench."
---   putStrLn "To the south is the main street,"
---   putStrLn "and the train station is to the east."
---   if hasItem "car_keys" state
---     then do
---       putStrLn "You have the car keys so you can try to open the abandoned car."
---       putStrLn "Type in enter_car in order to get into the vehicle."
---     else do
---       putStrLn ""
---   let updatedState = state {currentLocation = parking}
---   return updatedState
--- goNextLocation "car" state = do
---   putStrLn "You sit inside the car, but it refuses to start."
---   putStrLn "Type in exit_car in order to get out of the vehicle."
---   if isItemAt key "car" state
---     then do
---       putStrLn "You noticed an old key lying on the passenger seat."
---     else do
---       putStrLn ""
---   let updatedState = state {currentLocation = car}
---   return updatedState
 goNextLocation nextLocationName state =
   case findLocation nextLocationName of
     Just nextLocation ->
